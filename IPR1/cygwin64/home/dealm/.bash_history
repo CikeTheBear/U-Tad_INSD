27c554602f98e57f456e8bebc5368a136bc437ef
CikeTheBear <dealmeidaromanelli@gmail.com>
2025-10-27 16:10:04 -0400

    Cygwin added

---
+++ IPR1/cygwin64/home/dealm/.bash_history
@@ -0,0 +1,13 @@
+gcc
+gcc --version
+ls
+ls
+cd Programa_1/
+ls
+cat Ejercicio1.c 
+ls
+gcc Ejercicio1.c 
+gcc Ejercicio1.c -o Ejercicio.exe
+./ Ejercicio.exe
+./Ejercicio.exe
+exit
---
+++ IPR1/cygwin64/home/dealm/Programa_1/Ejercicio1.c
@@ -0,0 +1,27 @@
+//Librerias
+#include <stdio.h>
+
+//Declaraciones globales
+int numeroGlobal = 0;
+void miFuncion();
+
+
+
+//Main
+int main(){
+	
+	//Instrucciones del programa
+	printf("Hola, mi nombre es Carlos");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
