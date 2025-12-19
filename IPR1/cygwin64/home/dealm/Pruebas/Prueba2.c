023c92848d7b09bb9e0ca0df62c951952ee1e386
CikeTheBear <dealmeidaromanelli@gmail.com>
2025-12-19 19:17:56 -0400

    Add sample C programs and related binaries
    Cigwin reinstalled

---
+++ IPR1/cygwin64/home/dealm/Pruebas/Prueba2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main () {
+    int array[5] = {1, 2, 3, 4, 5};
+
+
+    printf("Elemento del array: %d\n", array[4]);
+
+
+}
---
+++ IPR1/cygwin64/home/dealm/Unidad_5/ejercicio1.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+
+
+
+int main() {
+	
+	int opcion = 0;
+	
+	
+	do {
+		printf("MENU: \n");
+		printf("1. GENERA UN PASSWORD\n");
+		printf("2. INTRODUCIR TU PASSWORD\n");
+		printf("3. SALIR\n");
+		
+		scanf("%d",&opcion);
+	}
+	while (opcion != 3);
+	
+	printf("%d", opcion);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// PSEUDOCODIGO
+/*
+BUCLE MIENTRAS OPCION SEA DISTINTA A 3 O 1 O 2
+MOSTRAR ("MENU: ")
+MOSTRAR ("1. GENERA PASSWORD")
+MOSTRAR ("2. INTRODUCIR PASSWORD")
+MOSTRAR ("3. SALIR")
+MOSTRAR ("SELECCIONE UNA OPCION")
+FIN BUCLE
+
+SI OPCION = 1
+	MOSTRAR ("INTRODUCE UN PASSWORD ENTRE 8 Y 15 CARACTERES Y AL MENOS UNA MAYUSCULA: ")
+	SWITCH
+		CASO 1 EL PASSWORD DEBE TENER AL MENOS UNA MAYUSCULA
+			MOSTRAR ("EL PASSWORD DEBE TENER AL MENOS UNA MAYUSCULA")
+		CASO 2 EL PASSWORD DEBE TENER ENTRE 8 Y 15 CARACTERES
+			MOSTRAR ("EL PASSWORD DEBE TENER ENTRE 8 Y 15 CARACTERES")
+		CASO 3 PASSWORD ACEPTADO
+			MOSTRAR ("PASSWORD ACEPTADO")
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+*/
