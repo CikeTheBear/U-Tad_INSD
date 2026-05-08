5d4583475696d0854ad7ef49bf4ee2551c328c1a
CikeTheBear <dealmeidaromanelli@gmail.com>
2026-05-08 15:18:16 -0400

    Create refuerzo-ejercicio-01.c

---
+++ IPR2/clases-refuerzo/refuerzo-ejercicio-01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define NUM_MAX_CARACTERES 16
+
+int validaLogin(char login[NUM_MAX_CARACTERES], int* numMay, int* numMin, int* numDig, int* numGuiones);
+
+void generaPassword(char login[NUM_MAX_CARACTERES], char password[NUM_MAX_CARACTERES]);
+
+int longitudCadena(char cadena[NUM_MAX_CARACTERES]);
+
+
+
+int main() {
+
+
+    return 0;
+}
